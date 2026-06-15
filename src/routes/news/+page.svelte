6ff0005ae352f7c63f68258a3d0171b317fde9b3
John Schoneboom <john.schoneboom@gmail.com>
2026-06-15 09:01:50 +0100

    adds HSSIB news item

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,20 +1,29 @@
+<script>
+  import { resolve } from '$app/paths';
+</script>
+
 <h1 class="eprase-h1">ePRaSE News</h1>
   
   <div class="accordion-text">
-    <p>Results from Version 4 (2025) of the Electronic Prescribing Risk and Safety Evaluation (ePRaSE) tool are now available as interactive, anonymised Power BI reports on <a href="/results/2025">the Results 2025 page</a> of this website. Trusts that completed the 2025 assessment have been emailed unique identification numbers, enabling benchmarking against other trusts using the same electronic prescribing (EP) system. Explanatory notes are provided to support interpretation of the data.</p>
+    <h2>HSSIB Recognises ePRaSE as Key Tool</h2>
+    <p>8 June 2026 -- The latest report from the Health Services Safety Investigations Body (HSSIB) examines the safety of electronic prescribing and medicines administration (ePMA) systems used across NHS acute hospitals and highlights the importance of robust mechanisms for identifying, assessing and learning from digital medication safety risks. Within the report, ePRaSE is cited as an important tool that NHS organisations can use to assess whether their ePMA systems are functioning as intended and supporting safe prescribing in practice.</p>
+
+    <p>[<a href={resolve("/pdf/hssib_news.pdf")} target="_blank">Read the full press release</a>]</p>
+    <h2>2025 Results Available</h2>
+    <p>Results from Version 4 (2025) of the Electronic Prescribing Risk and Safety Evaluation (ePRaSE) tool are now available as interactive, anonymised Power BI reports on <a href={resolve("/results/2025")}>the Results 2025 page</a> of this website. Trusts that completed the 2025 assessment have been emailed unique identification numbers, enabling benchmarking against other trusts using the same electronic prescribing (EP) system. Explanatory notes are provided to support interpretation of the data.</p>
 
     <p>To support interpretation and response to your ePRaSE results, we recommend the following resources:</p>
 
     <ul>
-      <li><a href="/using">User experience video</a>: Responding to ePRaSE, Andrew Heed</li>
-      <li><a href="/lab">Electronic Prescribing Learning Lab</a>: An interactive resource designed to help digital and pharmacy teams optimise their electronic prescribing systems.</li>
+      <li><a href={resolve("/using")}>User experience video</a>: Responding to ePRaSE, Andrew Heed</li>
+      <li><a href={resolve("/lab")}>Electronic Prescribing Learning Lab</a>: An interactive resource designed to help digital and pharmacy teams optimise their electronic prescribing systems.</li>
     </ul>
 
     <p>The Learning Lab is an interactive resource designed to help digital and pharmacy teams optimise their electronic prescribing systems.</p>
 
     <p>The ePRaSE team are already working on the next version of the tool, planned for release early Autumn 2026. If you have not previously had the chance to participate and would like to next time, please register to receive email updates via the QR code below.</p>
 
-    <img class='qrCode' src="/img/eprase_qr.png" alt="qr code" />
+    <img class='qrCode' src={resolve("/img/eprase_qr.png")} alt="qr code" />
   </div>
 
   <style>
