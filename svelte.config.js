85e1af73c37573da37684d88642328878ff1e0ed
John Schoneboom <john.schoneboom@gmail.com>
2026-09-02 11:55:07 +0100

    update deployment config

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import strict from 'node:assert/strict';
 
 /** @type {import('@sveltejs/kit').Config} */
-
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true,
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/eprase_info_staging' : '',
-			relative: false
-		}
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            precompress: false,
+            strict: true,
+            fallback: '404.html'
+        }),
+        paths: {
+            // Set base to empty string so assets resolve relative to domain root (eprase.info)
+            base: '',
+            relative: true
+        }
+    }
 };
 
 export default config;
